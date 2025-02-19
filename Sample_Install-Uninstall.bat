adfdb9627cd69971e56290614774b118eed9cf80
JohnTharma <john.tharmalingam@gmail.com>
2025-02-19 23:47:09 +0800

    Full Update of Sample_Install-Uninstall.bat

--- Sample_Install-Uninstall.bat
+++ Sample_Install-Uninstall.bat
@@ -1,3 +1,5 @@
+REM This is the Installation script
+
 :: It launches on 64bit CMD and continues installation of 64bit software
 
 IF NOT "%PROCESSOR_ARCHITEW6432%"=="AMD64" GOTO native
@@ -36,17 +38,57 @@ xcopy /Y /I ".\sampleapp\*" "C:\samplefolder\sampleapp" /E /H /C
 REM Add entry to registry
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%AppName% /V DisplayName /t REG_SZ /d "%AppName%" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%AppName% /V DisplayVersion /t REG_SZ /d "%AppVersion%" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%AppName% /V UninstallString /t REG_SZ /d "reg delete \"HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%AppName%\" f" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%AppName% /V UninstallString /t REG_SZ /d "reg delete \"HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%AppName%\" /f" /f
 
-REM Clear previous uninstallation log
+REM Clear previous Uninstallation log
 del /s /q "C:\Temp\Logs\%AppName% - Uninstall.log"
 
-Rem Log installation completion
+Rem Log Installation completion
 echo Installed "%AppName%" >> "%AppLog%\%AppName% - Install.log"
 
 timeout /t 30
 
 exit /b
+
+REM This is the Uninstallation script
+
+:: It launches on 64bit CMD and continues installation of 64bit software
+
+IF NOT "%PROCESSOR_ARCHITEW6432%"=="AMD64" GOTO native
+ECHO "Re-launching Script in Native Command Processor..."
+
+EXIT
+
+:native
+@echo off
+cd /d %~dp0
+pushd %~dp0
+setlocal ENABLEDELAYEDEXPANSION
+
+Rem set the application name (adjust as per your actual application name)
+set AppName=Sample App
+
+REM Remove Installation directory and shortcuts
+rmdir /s /q "C:\SampleFolder\Sampleapp" 2>nul
+DEL "C:\Users\Public\Desktop\sampleapp.ink" /q
+
+REM Run Uninstaller(For .exe)
+"C:\SampleFolder\Sampleapp\unins000.exe" /allusers /verysilent /norestart -i /S
+
+REM Uninstall application silently(MSI)
+start /wait msiexec.exe /x {msi string} /qn ALLUSERS=1 /l*v "%logfile%" /norestart
+
+REM Clear Installation log
+rmdir /s /q "C:\Temp\Logs\%AppName%" 2>nul
+
+Rem Log Uninstallation completion
+echo Uninstalled "%AppName%" >> "%AppLog%\%AppName% - Uninstall.log"
+
+REM Cleanup Registry
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%AppName%\" /f
+
+
+
 
 
 
